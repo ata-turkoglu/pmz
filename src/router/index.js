b3892ef5834aec98a8513056e60359451784e90e
ata_turkoglu <ata.turkoglu@reengen.com>
2023-03-01 22:12:42 +0300

    add login and signin paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
+import SignIn from "../views/SignIn";
+import Login from "../views/Login";
 import HomeView from "../views/HomeView";
 import NewActivityForm from "../views/ActivityForms/NewActivityForm.vue";
 import ActivityFormList from "../views/ActivityForms/ActivityFormList.vue";
 import Graphs from "../views/Graphs/Graphs";
 
 const routes = [
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
   {
     path: "/",
     name: "home",
