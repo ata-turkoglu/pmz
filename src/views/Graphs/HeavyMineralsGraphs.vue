24cd1d67822541b616bec55142664cc881b7cee9
ata_turkoglu <ata.turkoglu@reengen.com>
2023-03-08 23:24:10 +0300

    refactor add chart page

--- src/views/Graphs/HeavyMineralsGraphs.vue
+++ src/views/Graphs/HeavyMineralsGraphs.vue
@@ -10,11 +10,20 @@
         :enable-time-picker="false"
         position="left"
         :format="vdpInputFormat"
+        placeholder="Tarih Seç"
       >
         <template #action-select>
           <p class="vdp-select" @click="selectDate">Seç</p>
         </template>
       </VueDatePicker>
+      <div class="average-data">
+        <div>
+          <span>Genel Ortalama</span>
+        </div>
+        <div>
+          <span>Seçilen tarihlerde ortalama</span>
+        </div>
+      </div>
     </div>
     <HeavyMineralsConsumptionChart
       v-if="consumptionChartState"
@@ -54,6 +63,7 @@ export default {
   computed: {
     ...mapGetters({
       formList: "getHeavyMineralsForms",
+      chartData: "chartData/getHeavyMineralsConsumptionData",
     }),
   },
   methods: {
@@ -70,7 +80,7 @@ export default {
           range: [moment().subtract(7, "days").format(), moment().format()],
         },
         {
-          label: " 1_Ay ",
+          label: " Son_1_Ay ",
           range: [moment().subtract(1, "months").format(), moment().format()],
         },
         {
@@ -84,188 +94,79 @@ export default {
       let toDate = moment(endDate);
       let diff = toDate.diff(fromDate, type);
       let range = [];
-      for (let i = 0; i < diff; i++) {
+      for (let i = 0; i <= diff; i++) {
         range.push(moment(startDate).add(i, type));
       }
       return range;
     },
-    setDataByDateRange(range) {
-      //activity list day by day between selected dates
-      let list = this.formList
-        .filter(
-          (item) =>
-            moment(item.date) > range[0] &&
-            moment(item.date) < range[range.length - 1] &&
-            item.shift == 3
-        )
-        .reverse();
-
-      let calculatedList = list.map((el, index) => {
+    setDataByDateRange() {
+      return this.chartData.map((item) => {
         return {
-          cngTimer: el.cngTimer,
-          cng: list[index - 1] ? el.cngTimer - list[index - 1].cngTimer : 0,
-          date: el.date,
-          dryerKilnTimer: el.dryerKilnTimer,
-          dryerWorkingTime: list[index - 1]
-            ? el.dryerKilnTimer - list[index - 1].dryerKilnTimer
-            : 0,
-          reducerKilnTimer: el.reducerKilnTimer,
-          reducerWorkingTime: list[index - 1]
-            ? el.reducerKilnTimer - list[index - 1].reducerKilnTimer
-            : 0,
+          date: item.date,
+          dryer: {
+            workingTime: item.dryerWorkingTime,
+            totalConsumption: item.dryerWorkingTime * this.dryerConsumption,
+            hourlyAvarageConsumption:
+              +(
+                (item.dryerWorkingTime * this.dryerConsumption) /
+                item.dryerWorkingTime
+              ).toFixed(2) || 0,
+          },
+          reducer: {
+            workingTime: item.reducerWorkingTime,
+            totalConsumption:
+              item.cngConsumption -
+              item.dryerWorkingTime * this.dryerConsumption,
+            hourlyAvarageConsumption:
+              +(
+                (item.cngConsumption -
+                  item.dryerWorkingTime * this.dryerConsumption) /
+                item.reducerWorkingTime
+              ).toFixed(2) || 0,
+          },
         };
       });
-
-      let rangeData = [];
-
-      range
-        .map((itm) => itm.format("L").split(".").reverse().join("-"))
-        .forEach((el) => {
-          let foundDay = calculatedList.find((itm) => itm.date == el);
-          if (foundDay) {
-            rangeData.push({
-              date: foundDay.date,
-              dryer: {
-                workingTime: foundDay.dryerWorkingTime,
-                totalConsumption:
-                  foundDay.dryerWorkingTime * this.dryerConsumption,
-                hourlyAvarageConsumption:
-                  +(
-                    (foundDay.dryerWorkingTime * this.dryerConsumption) /
-                    foundDay.dryerWorkingTime
-                  ).toFixed(2) || 0,
-              },
-              reducer: {
-                workingTime: foundDay.reducerWorkingTime,
-                totalConsumption:
-                  foundDay.cng -
-                  foundDay.dryerWorkingTime * this.dryerConsumption,
-                hourlyAvarageConsumption:
-                  +(
-                    (foundDay.cng -
-                      foundDay.dryerWorkingTime * this.dryerConsumption) /
-                    foundDay.reducerWorkingTime
-                  ).toFixed(2) || 0,
-              },
-            });
-          } else {
-            rangeData.push({
-              date: el.date,
-              dryer: {
-                workingTime: 0,
-                totalConsumption: 0,
-                hourlyAvarageConsumption: 0,
-              },
-              reducer: {
-                workingTime: 0,
-                totalConsumption: 0,
-                hourlyAvarageConsumption: 0,
-              },
-            });
-          }
-        });
-
-      //calculation of day data
-      /*       let calculatedList = list.map((el, index) => {
-        let obj = {
-          cngTimer: el.cngTimer,
-          cng: list[index - 1] ? el.cngTimer - list[index - 1].cngTimer : null,
-          date: el.date,
-          dryerKilnTimer: el.dryerKilnTimer,
-          dryerWorkingTime: list[index - 1]
-            ? el.dryerKilnTimer - list[index - 1].dryerKilnTimer
-            : null,
-          reducerKilnTimer: el.reducerKilnTimer,
-          reducerWorkingTime: list[index - 1]
-            ? el.reducerKilnTimer - list[index - 1].reducerKilnTimer
-            : null,
-        };
-        return {
-          date: obj.date,
-          dryerWorkingTime: obj.dryerWorkingTime,
-          dryerTotalConsumption: obj.dryerWorkingTime * this.dryerConsumption,
-          dryerHourlyAvarageConsumption: +(
-            (obj.dryerWorkingTime * this.dryerConsumption) /
-            obj.dryerWorkingTime
-          ).toFixed(2),
-          reducerWorkingTime: obj.reducerWorkingTime,
-          reducerTotalConsumption:
-            obj.cng - obj.dryerWorkingTime * this.dryerConsumption,
-          reducerHourlyAvarageConsumption: +(
-            (obj.cng - obj.dryerWorkingTime * this.dryerConsumption) /
-            obj.reducerWorkingTime
-          ).toFixed(2),
-        };
-      }); */
-      //return calculatedList.slice(1);
-
-      return rangeData;
     },
     setXAxis() {
       this.consumptionChartState = false;
       setTimeout(() => {
+        //let startDate = moment(this.selectedDateRange[0]).subtract(1, "days");
         let startDate = moment(this.selectedDateRange[0]);
         let endDate = moment(this.selectedDateRange[1]);
-        let diff = endDate.diff(startDate, "days");
 
         let range = this.getRange(startDate, endDate, "days");
 
-        if (diff <= 7) {
+        if (range.length <= 7 && false) {
           //set week days
           this.consumption_xAxis = range.map((itm) =>
             moment.weekdays(true, itm.weekday()).substring(0, 2)
           );
-        } else if (diff <= 31 && diff > 7) {
+        } else if (range.length <= 31 && range.length > 7) {
           this.consumption_xAxis = range.map((itm) => itm.date());
         } else {
           this.consumption_xAxis = range.map((itm) => itm.format("DD/MM"));
         }
 
-        //calculate data
-        this.consumption_calculatedData = this.setDataByDateRange(range);
         this.consumptionChartState = true;
       }, 100);
     },
-    setLastWeekData() {
-      //start date
-      let startDate = moment().subtract(13, "days").format();
-
-      // date range
-      let range = this.getRange(
-        startDate,
-        moment().subtract(6, "days").format(),
-        "days"
-      );
-
-      //set week days
-      this.consumption_xAxis = range
-        .slice(1)
-        .map((itm) => moment.weekdays(true, itm.weekday()).substring(0, 2));
-
-      //calculate data
-      this.consumption_calculatedData = this.setDataByDateRange(
-        range[0],
-        range[range.length - 1]
-      );
-      this.consumptionChartState = true;
-    },
   },
   created() {
-    if (
-      this.$store.state.activityForms == [] ||
-      this.$store.state.activityForms.length <= 0
-    ) {
-      this.$store.dispatch("getFacilityActivityForms", 2).then(() => {
-        this.setPresentRanges();
-      });
-    } else {
-      this.setPresentRanges();
-    }
+    this.setPresentRanges();
   },
   watch: {
     selectedDateRange: {
-      handler() {
-        this.setXAxis();
+      handler(val) {
+        this.$store
+          .dispatch("chartData/getDailyChartDataByDateRange", {
+            facility: 2,
+            startDate: moment(val[0]).format("YYYY-MM-DD"),
+            endDate: moment(val[1]).format("YYYY-MM-DD"),
+          })
+          .then(() => {
+            this.setXAxis();
+            this.consumption_calculatedData = this.setDataByDateRange();
+          });
       },
     },
   },
@@ -291,10 +192,16 @@ export default {
   cursor: pointer;
   font-weight: 600;
 }
+.average-data {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
 @media screen and (max-width: 600px) {
   .container {
     margin-inline: 0;
     padding-inline: 0;
+    min-height: 90vh;
   }
   .datepicker {
     width: 100%;
