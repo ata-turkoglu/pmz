2467db85dcabe167a27ea90ef5d28ef8bc9c297f
ata_turkoglu <ata.turkoglu@reengen.com>
2023-09-10 18:06:32 +0300

    add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-navigation-drawer
+        v-model="store.state.drawer"
+        class="px-3 py-2"
+        temporary
+        color="blue-darken-4"
+        width="30%"
+    >
+        <slot></slot>
+    </v-navigation-drawer>
+</template>
+<script>
+import { useStore } from "vuex";
+export default {
+    setup() {
+        const store = useStore();
+
+        return { store };
+    },
+};
+</script>
+<style scoped>
+.nav-list-item {
+    display: flex;
+    color: white;
+    cursor: pointer;
+}
+</style>
