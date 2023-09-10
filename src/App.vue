02e3846e8b566b473d1d4891571049d9b76fe191
ata_turkoglu <ata.turkoglu@reengen.com>
2023-09-10 18:05:54 +0300

    initial index file

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="main-container">
-    <nav></nav>
-    <router-view />
-    <v-dialog
-      v-model="$store.state.commonDialogs.routePermissionDialog"
-      width="auto"
-    >
-      <ErrorDialog
-        :errorText="'Giriş için izniniz yok'"
-        @close="$store.state.commonDialogs.routePermissionDialog = false"
-      />
-    </v-dialog>
-  </div>
+    <div class="main-container">
+        <nav></nav>
+        <router-view />
+        <v-dialog
+            v-model="$store.state.commonDialogs.routePermissionDialog"
+            width="auto"
+        >
+            <ErrorDialog
+                :errorText="'Giriş için izniniz yok'"
+                @close="
+                    $store.state.commonDialogs.routePermissionDialog = false
+                "
+            />
+        </v-dialog>
+    </div>
 </template>
 
 <script>
 import ErrorDialog from "./components/common/ErrorDialog.vue";
 export default {
-  components: { ErrorDialog },
-  data: () => ({}),
-  created() {
-    //this.$store.dispatch("getAllFacilities");
-  },
-  beforeCreate() {
-    this.$store.dispatch("users/userExist").then(() => {
-      !this.$store.state.users.authenticated && this.$router.push("/login");
-    });
-  },
+    components: { ErrorDialog },
+    data: () => ({}),
+    created() {
+        //this.$store.dispatch("getAllFacilities");
+    },
+    beforeCreate() {
+        this.$store.dispatch("users/userExist").then(() => {
+            !this.$store.state.users.authenticated &&
+                this.$router.push("/login");
+        });
+    },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 .main-container {
-  width: 100%;
-  height: 100%;
-  min-height: 90vh;
+    width: 100%;
+    height: 100%;
+    height: 100vh;
 }
 nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
